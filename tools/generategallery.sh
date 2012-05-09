465947f248068c6678238b292c40fc2ae86ce4c9
neno-giscloud <neno@giscloud.com>
2012-05-09 11:41:37 +0100

    Added a gallery creation script. Added a script to render a single icon to png with or without the glow or invert effects.

---
+++ tools/generategallery.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# $1 size
+# $2 colour
+
+pushd . > /dev/null
+cd `dirname $BASH_SOURCE` > /dev/null
+BASEFOLDER=`pwd`;
+popd  > /dev/null
+BASEFOLDER=`dirname $BASEFOLDER`
+
+TYPES=('accommodation' 'amenity' 'barrier' 'education' 'food' 'health' 'landuse' 'money' 'place_of_worship' 'poi' 'power' 'shopping' 'sport' 'tourist' 'transport' 'water')
+
+SVGFOLDER=${BASEFOLDER}/svg/
+OUTPUTFOLDER=${BASEFOLDER}/gallery/
+
+if [ ! -d "${OUTPUTFOLDER}" ]; then
+  mkdir ${OUTPUTFOLDER}
+fi
+
+for (( i = 0 ; i < ${#TYPES[@]} ; i++ )) do
+
+    echo "On: ${TYPES[i]}"
+
+    for FILE in $SVGFOLDER${TYPES[i]}/*.svg; do
+
+      BASENAME=${FILE##/*/}
+      BASENAME=${OUTPUTFOLDER}${TYPES[i]}/${BASENAME%.*}
+
+      if [ ! -d "${OUTPUTFOLDER}${TYPES[i]}" ]; then
+        mkdir ${OUTPUTFOLDER}${TYPES[i]}
+      fi
+      
+      ${BASEFOLDER}/tools/recolourtopng.sh ${FILE} 'none' 'none' $2 $1 ${BASENAME}.$1
+      
+    done
+
+done
---
+++ tools/generatesingle.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# $1 type - like 'accommodation' or 'amenity'
+# $2 name - name of the icon without the '.svg'
+# $3 colour - e.g. '#ffddaa'
+# $4 size - size in pixels e.g. 32
+# $5 output path - if empty string, working dir will be used
+# $6 effect - 'invert' OR 'glow'
+
+pushd . > /dev/null
+cd `dirname $BASH_SOURCE` > /dev/null
+BASEFOLDER=`pwd`;
+popd  > /dev/null
+BASEFOLDER=`dirname $BASEFOLDER`
+
+INPUTFILE="${BASEFOLDER}/svg/$1/$2.svg"
+
+OUTPUTNAME="$1.$2.$3.$4"
+
+if [ -z "$5" ]; then
+  OUTPUTFOLDER=${BASEFOLDER}/
+fi
+
+if [ ! -d "${OUTPUTFOLDER}" ]; then
+  mkdir ${OUTPUTFOLDER}
+fi
+
+if [ "$6" == "invert" ]; then
+  OUTPUTNAME="$OUTPUTNAME.invert"
+  ${BASEFOLDER}/tools/recolourtopng.sh ${INPUTFILE} $3 $3 '#ffffff' $4 ${OUTPUTFOLDER}/${OUTPUTNAME}
+elif [ "$6" == "glow" ]; then
+  OUTPUTNAME="$OUTPUTNAME.glow"
+  ${BASEFOLDER}/tools/recolourtopng.sh ${INPUTFILE} 'none' 'none' $3 $4 ${OUTPUTFOLDER}/${OUTPUTNAME}.tmp
+  convert ${OUTPUTFOLDER}/${OUTPUTNAME}.tmp.png \( +clone -background "#ffffff" -shadow 8000x2-0+0 \) +swap -background none -layers merge +repage -trim ${OUTPUTFOLDER}/${OUTPUTNAME}.png
+  rm ${OUTPUTFOLDER}/${OUTPUTNAME}.tmp.png
+else
+  ${BASEFOLDER}/tools/recolourtopng.sh ${INPUTFILE} 'none' 'none' $3 $4 ${OUTPUTFOLDER}/${OUTPUTNAME}
+fi
